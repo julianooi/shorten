0e8f8056cbe5d268c43c8e0965fc558178fa5335
Julian Ooi <julianooi@gmail.com>
2022-06-29 16:01:45 +0800

    implement in memory shortener

---
+++ memory/memory.go
@@ -0,0 +1,96 @@
+package memory
+
+import (
+	"github.com/julianooi/shorten"
+	"sync"
+	"time"
+)
+
+type status struct {
+	url       string
+	createdAt time.Time
+	mu        sync.Mutex
+	count     int
+}
+
+func (s *status) increment() {
+	s.mu.Lock()
+	s.count++
+	s.mu.Unlock()
+}
+
+type Shortener struct {
+	store map[string]status
+	mu    sync.Mutex
+	count int
+}
+
+func NewShortener() *Shortener {
+	return &Shortener{
+		store: make(map[string]status),
+	}
+}
+
+func (s *Shortener) nextKey() string {
+	s.mu.Lock()
+	c := s.count
+	s.count++
+	s.mu.Unlock()
+	return generateKey(c)
+}
+
+var validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+func generateKey(count int) string {
+	l := len(validChars)
+	output := ""
+
+	output += string(validChars[count%l])
+	prependCount := count / l
+	for i := 0; i < prependCount; i++ {
+		output += string(validChars[l-1])
+	}
+
+	toAppend := 6 - len(output)
+	if toAppend < 0 {
+		panic("more characters than support[6]")
+	}
+
+	for i := 0; i < toAppend; i++ {
+		output += "-"
+	}
+
+	return output
+}
+
+func (s *Shortener) Shorten(url string) (string, error) {
+	key := s.nextKey()
+	s.store[key] = status{
+		url:       url,
+		createdAt: time.Now(),
+		mu:        sync.Mutex{},
+		count:     0,
+	}
+	return key, nil
+}
+
+func (s *Shortener) Status(key string) (shorten.Status, error) {
+	state, ok := s.store[key]
+	if !ok {
+		return shorten.Status{}, shorten.ErrKeyNotFound
+	}
+	return shorten.Status{
+		URL:       state.url,
+		Count:     state.count,
+		CreatedAt: state.createdAt,
+	}, nil
+}
+
+func (s *Shortener) Increment(key string) error {
+	state, ok := s.store[key]
+	if !ok {
+		return shorten.ErrKeyNotFound
+	}
+	state.increment()
+	return nil
+}
---
+++ memory/memory_test.go
@@ -0,0 +1,59 @@
+package memory
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGenerateKey(t *testing.T) {
+	tt := []struct {
+		count    int
+		expected string
+	}{
+		{
+			count:    0,
+			expected: "A-----",
+		},
+		{
+			count:    10,
+			expected: "K-----",
+		},
+		{
+			count:    124,
+			expected: "A99---",
+		},
+		{
+			count:    371,
+			expected: "999999",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("count: %d", tc.count), func(t *testing.T) {
+			key := generateKey(tc.count)
+			if key != tc.expected {
+				t.Errorf("expected [%s], got [%s]", tc.expected, key)
+			}
+		})
+	}
+}
+
+func TestShortener_Shorten_Success(t *testing.T) {
+	tt := []struct {
+		url string
+	}{
+		{url: "https://www.example.com/long-test-url"},
+	}
+	for _, tc := range tt {
+		t.Run(tc.url, func(t *testing.T) {
+			shortener := NewShortener()
+			key, err := shortener.Shorten(tc.url)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+			if len(key) != 6 {
+				t.Errorf("unexpected key length, expected 6")
+			}
+		})
+	}
+}
